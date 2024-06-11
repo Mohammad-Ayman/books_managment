aebd32aef930c020dad89a012db0896bd7fe8f07
Mohammad-Ayman <mohdayman22rr@gmail.com>
2024-06-11 21:19:30 +0300

    chore: Add logging middleware to save request logs to the database

---
+++ internal/models/logModel.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+    "time"
+	"net/http"
+	"log"
+
+    "github.com/jinzhu/gorm"
+	"github.com/mohammad-ayman/books_managment/internal/config"
+)
+
+type RequestLog struct {
+    gorm.Model
+    Method      string    `json:"method"`
+    RequestURI  string    `json:"request_uri"`
+	UserIP	  string    `json:"user_ip"`
+    Timestamp   time.Time `json:"timestamp"`
+}
+
+func init() {
+	config.InitializeDatabase()
+	db = config.GetDB()
+	db.AutoMigrate(&RequestLog{})
+}
+
+
+// logRequestToDB saves information about the incoming request to the database
+func LogRequestToDB(r *http.Request) {
+
+    db := config.GetDB()
+
+    // Create a new request log entry
+    requestLog := RequestLog{
+        Method:     r.Method,
+        RequestURI: r.RequestURI,
+		UserIP:     r.RemoteAddr,
+        Timestamp:  time.Now(),
+    }
+
+    // Save the request log to the database
+    if err := db.Create(&requestLog).Error; err != nil {
+        log.Printf("Error saving request log to database: %v", err)
+    }
+}
