f061e5881116ab1e8aa29eff84bc649df522a693
Mohammad-Ayman <mohdayman22rr@gmail.com>
2024-06-11 19:46:06 +0300

    chore: Update main.go with environment variable handling and API subrouter

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+// main.go
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/gorilla/mux"
+    "github.com/joho/godotenv"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+
+    "github.com/mohammad-ayman/books_managment/internal/routes"
+    "github.com/mohammad-ayman/books_managment/internal/config"
+    )    
+
+
+func main() {    
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" 
+    }
+
+    // Initialize the database
+    config.InitializeDatabase()
+    router := mux.NewRouter()
+
+	// Create a subrouter for API requests
+	apiRouter := router.PathPrefix("/api").Subrouter()
+	
+	// Register routes for the API subrouter
+	routes.BooksManagmentRoutes(apiRouter)
+    
+    fmt.Println("server running on port " + port)
+    log.Fatal(http.ListenAndServe(":"+port, router))
+}
